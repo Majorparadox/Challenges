2586ea750cef247090a71e8c282f5c375042a239
Majorparadox <42531187+Majorparadox@users.noreply.github.com>
2018-08-22 21:45:26 +0800

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,46 @@
+import turtle
+
+print("1. \n")
+like = ("I like turtle \n")
+print(like*1000)
+
+print("2. \n")
+
+print("3. \n")
+Month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+for i in Month:
+    print("One of the month's of the year is:", i)
+
+# Unrelated looping question. Is there any way to return a certain part of a list based on user input?
+
+print("4. \n")
+tess = turtle.Turtle
+wn = turtle.Screen()
+tess.left(3645)
+
+print("5. \n")
+
+print("6. \n")
+
+print("7. \n")
+
+print("7. \n")
+
+print("8. \n")
+
+print("9. \n")
+
+print("10. \n")
+
+print("11. \n")
+
+print("12. \n")
+
+print("13. \n")
+
+print("14. \n")
+
+print("15. \n")
+
+
+
