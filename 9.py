9c873794da41b71eb3bd0fc93d8139bde6994adc
Majorparadox <42531187+Majorparadox@users.noreply.github.com>
2018-11-03 16:56:49 +0800

    Create 9.py

---
+++ 9.py
@@ -0,0 +1,13 @@
+print("1. \n")
+Letters = {"A":1, "B":2, "C":3, "D":4, "E":5}
+
+def test(f):
+    print(f["A"])
+test(Letters)
+#It's possible
+
+print("2. \n")
+#Tuple is a generalization of a pair
+
+print("3. \n")
+#Tuple is a kind of a pair
