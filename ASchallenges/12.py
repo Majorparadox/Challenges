e7cdd0f71205477d67eacfc5f8a7cf530076809e
Majorparadox <42531187+Majorparadox@users.noreply.github.com>
2018-11-17 10:53:17 +0800

    Create 12.py

---
+++ ASchallenges/12.py
@@ -0,0 +1,47 @@
+print("1 \n")
+import calendar
+cal = calendar.TextCalendar()      # Create an instance
+cal.pryear(2012)
+
+#b
+
+#c
+mon = calendar.TextCalendar()
+mon.prmonth(2019, 1)
+
+#d
+d = calendar.LocaleTextCalendar(6, "SPANISH")
+d.pryear(2012)
+
+#e returns True or False depending on if the year is a leap year
+
+print("2 \n")
+#a 49
+
+#b returns the ceiling of a variable as a float
+
+#c returns the floor of a variablee as a float
+
+#d pi and
+
+print("3 \n")
+# It adds copies of each object into a new one
+
+print("4 \n")
+print( (mymodule2.myage - mymodule1.myage) ==
+       (mymodule2.year - mymodule1.year)  )
+
+print("5 \n")
+# It outputs "My name is __main__"
+
+print("6 \n")
+"Namespaces are one honking great idea -- let's do more of those!"
+
+print("7 \n")
+'''
+['If', 'we', 'took', 'the', 'bones', 'out,', 'it', "wouldn't", 'be', 'crunchy,', 'would', 'it?']
+<class 'list'>
+['If we t', '', 'k the b', 'nes ', 'ut, it w', "uldn't be crunchy, w", 'uld it?']
+['If we took the bones out, ', "t wouldn't be crunchy, would ", 't?']
+If we t00k the b0nes 0ut, it w0uldn't be crunchy, w0uld it?
+'''
